b0d642410d213880db98d8728ac63b2bf7451a2d
conrado <conradeleon11@gmail.com>
2026-05-11 22:56:09 -0300

    Create and connect Booking Form with API endpoint

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FlightService } from './services/flight';
 import { FlightSearchResponse } from './models/flight.model';
 import { CommonModule } from '@angular/common';
+import { BookingRequest } from './models/flight.model';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +18,8 @@ export class AppComponent {
   selectedFlight: FlightSearchResponse | null = null;
   flights = signal<FlightSearchResponse[]>([]);
   loading = signal<boolean>(false);
+  isBooking = signal<boolean>(false);
+  bookingStatus = signal<string | null>(null);
 
   searchForm = this.fb.group({
     origin: ['', [Validators.required, Validators.minLength(3)]],
@@ -26,12 +29,19 @@ export class AppComponent {
     cabinClass: ['Economy', Validators.required]
   });
 
+  bookingForm = this.fb.group({
+    fullName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    documentNumber: ['', Validators.required]
+  });
+
   onSearch() {
     if (this.searchForm.invalid) return;
 
     this.loading.set(true);
     this.flights.set([]);
-    
+    this.bookingStatus.set(null);
+
     const val = this.searchForm.value;
 
     this.flightService.searchFlights(
@@ -56,6 +66,43 @@ export class AppComponent {
 
   onBook(flight: FlightSearchResponse) {
     this.selectedFlight = flight;
-    console.log('User selected flight:', flight.flightNumber);
+    this.isBooking.set(true);
+    this.bookingStatus.set(null);
+  }
+  confirmBooking() {
+    if (this.bookingForm.invalid || !this.selectedFlight) return;
+
+    this.loading.set(true);
+
+    const request: BookingRequest = {
+      flightNumber: this.selectedFlight.flightNumber,
+      providerName: this.selectedFlight.providerName,
+      fullName: this.bookingForm.value.fullName!,
+      email: this.bookingForm.value.email!,
+      documentNumber: this.bookingForm.value.documentNumber!
+    };
+
+    this.flightService.bookFlight(request).subscribe({
+      next: (res) => {
+        this.bookingStatus.set(`Success! Ref: ${res.bookingReference}`);
+        this.isBooking.set(false);
+        this.loading.set(false);
+        this.flights.set([]);
+
+        this.bookingForm.reset();
+        this.selectedFlight = null;
+      },
+      error: (err) => {
+        console.error('Booking error', err);
+        this.bookingStatus.set('Error processing your booking.');
+        this.loading.set(false);
+      }
+    });
+  }
+
+  cancelBooking() {
+    this.isBooking.set(false);
+    this.selectedFlight = null;
+    this.bookingForm.reset();
   }
 }
